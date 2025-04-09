d6f96b1f071c92e0a40ca137645b52b36c412c20
OhPointFive <ohpointfive@gmail.com>
2025-04-09 00:41:53 -0400

    Add tests

---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, test, expect } from 'vitest';
+import { testFunction } from '../src';
+
+describe("index", () => {
+    test("should return hello world string", () => {
+        expect(testFunction()).toBe("Hello, world!");
+    });
+});
