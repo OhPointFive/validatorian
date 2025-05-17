d189151c0341a5940ca034209e9134097b8e889f
OhPointFive <ohpointfive@gmail.com>
2025-05-17 13:50:12 -0400

    Reformat string validators

--- src/validators/string.ts
+++ src/validators/string.ts
@@ -1,13 +1,17 @@
 import { booleanValidator } from "../validator-helpers/validator";
 
 /** Validates that the passed value is a string. */
-export const vString = booleanValidator("string", (value: unknown): value is string =>
-    typeof value === "string",
+export const vString = booleanValidator(
+    "string",
+    (value: unknown): value is string =>
+        typeof value === "string",
 );
 
 /** Validates that the passed value is a non-empty string. */
-export const vNonEmptyString = booleanValidator("non-empty string", (value: unknown): value is string =>
-    typeof value === "string" && value.length > 0,
+export const vNonEmptyString = booleanValidator(
+    "non-empty string",
+    (value: unknown): value is string =>
+        typeof value === "string" && value.length > 0,
 );
 
 /**
@@ -22,14 +26,8 @@ export const vNonEmptyString = booleanValidator("non-empty string", (value: unkn
  * See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length
  * for details.
  */
-export const vStringOfLength = (minLength: number, maxLength: number = minLength) =>
-    booleanValidator(
-        minLength === maxLength ? `string of length ${minLength}` : `string of length [${minLength}, ${maxLength}]`,
-        (value: unknown): value is string => {
-            if (typeof value !== "string") {
-                return false;
-            }
-            const length = value.length;
-            return length >= minLength && length <= maxLength;
-        },
-    );
+export const vStringOfLength = (minLength: number, maxLength: number = minLength) => booleanValidator(
+    minLength === maxLength ? `string of length ${minLength}` : `string of length [${minLength}, ${maxLength}]`,
+    (value: unknown): value is string =>
+        typeof value === "string" && value.length >= minLength && value.length <= maxLength,
+);
