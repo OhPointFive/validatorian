382848ae4c49ce5a9f131353315392fae462196c
OhPointFive <ohpointfive@gmail.com>
2025-05-24 15:37:28 -0400

    Add const validator

---
+++ src/validators/const.ts
@@ -0,0 +1,9 @@
+import { booleanValidator } from "../validator-helpers/validator";
+
+/**
+ * Validates that the passed value is strictly equal to the specified value.
+ *
+ * For example, `vConst("Example")` will make a validator
+ * that only accepts the string `"Example"`.
+ */
+export const vConst = <const T>(value: T) => booleanValidator(`Exact ${typeof value} \`${value}\``, (v: unknown): v is T => v === value);
