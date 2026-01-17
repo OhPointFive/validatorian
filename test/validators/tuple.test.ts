b21c6d59cd655c210ef480dd5ff0091a089db411
OhPointFive <ohpointfive@gmail.com>
2026-01-17 12:40:12 -0500

    Add tuple tests

---
+++ test/validators/tuple.test.ts
@@ -0,0 +1,92 @@
+import { describe, test, expect } from "vitest";
+import { SingleValidationError } from "../../src/errors/single";
+import { vTuple, vStrictTuple } from "../../src/validators/tuple";
+import { vNumber } from "../../src/validators/number";
+import { vString } from "../../src/validators/string";
+import { vOrUndefined } from "../../src/validators/optional";
+import type { Validator } from "../../src/validator-helpers/validator";
+
+describe("vTuple", () => {
+    test("should exist", () => {
+        expect(vTuple).toBeDefined();
+    });
+
+    test("should validate tuples with correct items", () => {
+        expect(vTuple(vNumber, vString)([1, "test"])).toEqual([1, "test"]);
+        expect(vTuple(vString, vNumber, vString)(["a", 2, "c"])).toEqual(["a", 2, "c"]);
+    });
+
+    test("should handle too short arrays with undefined", () => {
+        expect(vTuple(vNumber, vOrUndefined(vString))([1])).toEqual([1, undefined]);
+        expect(vTuple(vOrUndefined(vNumber), vOrUndefined(vString))([])).toEqual([undefined, undefined]);
+    });
+
+    test("should drop extra items from too long arrays", () => {
+        expect(vTuple(vNumber, vString)([1, "test", "extra"])).toEqual([1, "test"]);
+        expect(vTuple(vNumber)([1, 2, 3, 4, 5])).toEqual([1]);
+    });
+
+    test("should throw for non-arrays", () => {
+        expect(() => vTuple(vNumber, vString)(1)).toThrow(new SingleValidationError("tuple", 1));
+        expect(() => vTuple(vNumber, vString)("test")).toThrow(new SingleValidationError("tuple", "test"));
+        expect(() => vTuple(vNumber, vString)(null)).toThrow(new SingleValidationError("tuple", null));
+        expect(() => vTuple(vNumber, vString)(undefined)).toThrow(new SingleValidationError("tuple", undefined));
+        expect(() => vTuple(vNumber, vString)({})).toThrow(new SingleValidationError("tuple", {}));
+    });
+
+    test("should throw for tuples with invalid items", () => {
+        expect(() => vTuple(vNumber, vString)([1, 2])).toThrow(new SingleValidationError("string", 2));
+        expect(() => vTuple(vString, vNumber)(["a", "b"])).toThrow(new SingleValidationError("number", "b"));
+    });
+
+    test("should pass type checking", () => {
+        const v1: Validator<[number, string]> = vTuple(vNumber, vString);
+        const result1: [number, string] = vTuple(vNumber, vString)([1, "test"] as unknown);
+
+        // @ts-expect-error
+        const result2: [string, number] = vTuple(vNumber, vString)([1, "test"] as unknown);
+    });
+});
+
+describe("vStrictTuple", () => {
+    test("should exist", () => {
+        expect(vStrictTuple).toBeDefined();
+    });
+
+    test("should validate tuples with correct items and length", () => {
+        expect(vStrictTuple(vNumber, vString)([1, "test"])).toEqual([1, "test"]);
+        expect(vStrictTuple(vString, vNumber, vString)(["a", 2, "c"])).toEqual(["a", 2, "c"]);
+    });
+
+    test("should throw for too short arrays", () => {
+        expect(() => vStrictTuple(vNumber, vString)([1])).toThrow(new SingleValidationError("strict tuple", [1]));
+        expect(() => vStrictTuple(vNumber, vString, vNumber)([1, "test"])).toThrow(new SingleValidationError("strict tuple", [1, "test"]));
+        expect(() => vStrictTuple(vNumber)([])).toThrow(new SingleValidationError("strict tuple", []));
+    });
+
+    test("should throw for too long arrays", () => {
+        expect(() => vStrictTuple(vNumber, vString)([1, "test", "extra"])).toThrow(new SingleValidationError("strict tuple", [1, "test", "extra"]));
+        expect(() => vStrictTuple(vNumber)([1, 2])).toThrow(new SingleValidationError("strict tuple", [1, 2]));
+    });
+
+    test("should throw for non-arrays", () => {
+        expect(() => vStrictTuple(vNumber, vString)(1)).toThrow(new SingleValidationError("strict tuple", 1));
+        expect(() => vStrictTuple(vNumber, vString)("test")).toThrow(new SingleValidationError("strict tuple", "test"));
+        expect(() => vStrictTuple(vNumber, vString)(null)).toThrow(new SingleValidationError("strict tuple", null));
+        expect(() => vStrictTuple(vNumber, vString)(undefined)).toThrow(new SingleValidationError("strict tuple", undefined));
+        expect(() => vStrictTuple(vNumber, vString)({})).toThrow(new SingleValidationError("strict tuple", {}));
+    });
+
+    test("should throw for tuples with invalid items", () => {
+        expect(() => vStrictTuple(vNumber, vString)([1, 2])).toThrow(new SingleValidationError("string", 2));
+        expect(() => vStrictTuple(vString, vNumber)(["a", "b"])).toThrow(new SingleValidationError("number", "b"));
+    });
+
+    test("should pass type checking", () => {
+        const v1: Validator<[number, string]> = vStrictTuple(vNumber, vString);
+        const result1: [number, string] = vStrictTuple(vNumber, vString)([1, "test"] as unknown);
+
+        // @ts-expect-error
+        const result2: [string, number] = vStrictTuple(vNumber, vString)([1, "test"] as unknown);
+    });
+});
