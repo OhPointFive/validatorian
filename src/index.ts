1c3ad41e207ea9a2d5201bf8a299f38c4b4e481f
OhPointFive <ohpointfive@gmail.com>
2026-01-18 03:19:52 -0500

    Export all the validators

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export function testFunction() {
-    return "Hello, world!";
-}
+export * from "./validators/array";
+export * from "./validators/const";
+export * from "./validators/default";
+export * from "./validators/number";
+export * from "./validators/object";
+export * from "./validators/optional";
+export * from "./validators/override";
+export * from "./validators/string";
+export * from "./validators/tuple";
+export * from "./validators/union";
